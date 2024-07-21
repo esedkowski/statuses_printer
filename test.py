5cdf38ffbaaa5ba8961e04c292eeb40ef0323192
Eryk <esedkowski@gmail.com>
2024-07-21 15:31:18 +0200

    some testing and looking around

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("test")
+from pylatex import Document, Section, Subsection
+
+doc=Document(documentclass='article')
+
+# creating a pdf with title "the simple stuff"
+with doc.create(Section('The simple stuff')):
+    doc.append('Some regular text and some')
+
+doc.generate_pdf('full', clean_tex=False)
+
